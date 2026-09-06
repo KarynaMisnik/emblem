26bbe36db5305c0c9367ac110f8d41bde08d5bbf
KarynaMisnik <karinamisnik94@gmail.com>
2026-09-06 10:20:57 +0300

    data folder created, regions.ts file added

---
+++ data/regions.ts
@@ -0,0 +1,133 @@
+export const regions = [
+  {
+    name: "Ahvenanmaa",
+    regionCode: "MK21",
+    regionDescription:
+      "Åland's coat of arms (a red deer) was mistakenly swapped with Öland's (two roe deer with roses). The error, discovered in th…ife of the islands, which were historically abundant with deer and other animals. It was designed by Ahti Hammarin in 1962.",
+    image: require("@/assets/Ahvenanmaa/Ahvenanmaa.png"),
+  },
+  {
+    name: "Etelä-Karjala",
+    regionCode: "MK09",
+    blazon:
+      "On a shield split horizontally in red and black, a wavy silver bar appears with two clashing arms above it. The right arm, a…holds a curved saber. All elements are silver, except for the golden hilts of the weapons and the joint rings on the armor.",
+    image: require("@/assets/Etelä-Karjala/Etelä-Karjala.png"),
+  },
+  {
+    name: "Etelä-Pohjanmaa",
+    regionCode: "MK14",
+    blazon:
+      "In a blue field, three silver ermines running in a row with black-tipped tails.",
+    image: require("@/assets/Etelä-Pohjanmaa/Etelä-Pohjanmaa.png"),
+  },
+  {
+    name: "Etelä-Savo",
+    regionCode: "MK10",
+    blazon:
+      "In a black field, a handbow set in tension, all in gold, except for the bowstring, arrowhead, and fletched tail, which are silver.",
+    image: require("@/assets/Etelä-Savo/Etelä-Savo.png"),
+  },
+  {
+    name: "Kainuu",
+    regionCode: "MK18",
+    blazon:
+      "In a gold field, a green fess embattled at the top and wavy at the bottom.",
+    image: require("@/assets/Kainuu/Kainuu.png"),
+  },
+  {
+    name: "Kanta-Häme",
+    regionCode: "MK05",
+    blazon:
+      "A golden lynx walking on a red field, with black ear tufts; accompanied by three six-pointed stars above the lynx and four roses below, arranged 1:3; all silver. On the shield is a ducal crown.",
+    image: require("@/assets/Kanta-Häme/Kanta-Häme.png"),
+  },
+  {
+    name: "Keski-Pohjanmaa",
+    regionCode: "MK16",
+    blazon:
+      "A silver marten with golden claws and tongue, looking backward, on a blue field",
+    image: require("@/assets/Keski-Pohjanmaa/Keski-Pohjanmaa.png"),
+  },
+  {
+    name: "Keski-Suomi",
+    regionCode: "MK13",
+    blazon: "In a silver field, a black, red-breasted capercaillie.",
+    image: require("@/assets/Keski-Suomi/Keski-Suomi.png"),
+  },
+  {
+    name: "Kymenlaakso",
+    regionCode: "MK08",
+    blazon:
+      "In a divided shield, the upper blue field features a silver salmon leaping upward, with golden details, and the lower silver field displays a blue net extending to the edges.",
+    image: require("@/assets/Kymenlaakso/Kymenlaakso.png"),
+  },
+  {
+    name: "Lappi",
+    regionCode: "MK19",
+    blazon:
+      "In a red field, a natural-colored wild man carrying a golden club over his shoulder; a green wreath adorns his forehead and waist. Above the shield is a comital coronet.",
+    image: require("@/assets/Lappi/Lappi.png"),
+  },
+  {
+    name: "Päijät-Häme",
+    regionCode: "MK07",
+    blazon:
+      "In a blue field, a mermaid and in the upper right corner, a cuckoo; all in gold.",
+    image: require("@/assets/Päijät-Häme/Päijät-Häme.png"),
+  },
+  {
+    name: "Pirkanmaa",
+    regionCode: "MK06",
+    blazon: "In a golden field, a red, fur-cornered cap.",
+    image: require("@/assets/Pirkanmaa/Pirkanmaa.png"),
+  },
+  {
+    name: "Pohjanmaa",
+    regionCode: "MK15",
+    blazon:
+      "In a red field, a golden royal fasces (the emblem of the House of Vasa) and a chief, also gold, with four red ermines courant arranged 2+2.",
+    image: require("@/assets/Pohjanmaa/Pohjanmaa.png"),
+  },
+  {
+    name: "Pohjois-Karjala",
+    regionCode: "MK12",
+    blazon:
+      "In a red field, two arms issuing from the sides and striking at each other, the right arm armored and wielding a straight sw…eft arm clad in chain mail and holding a curved saber; all in silver, with the weapon hilts in gold; above, a golden crown.",
+    image: require("@/assets/Pohjois-Karjala/Pohjois-Karjala.png"),
+  },
+  {
+    name: "Pohjois-Pohjanmaa",
+    regionCode: "MK17",
+    blazon:
+      "In a blue field, six running silver ermine, with black-tipped tails, arranged in a 2:2:2 pattern.",
+    image: require("@/assets/Pohjois-Pohjanmaa/Pohjois-Pohjanmaa.png"),
+  },
+  {
+    name: "Pohjois-Savo",
+    regionCode: "MK11",
+    blazon:
+      "On a black shield, a gold bow with a string. All in gold except the bowstring, the arrowhead, and the feathered tail, which … pearls on the crown's rim and three pearls above it, one in the middle and one above each of the outermost visible pearls.",
+    image: require("@/assets/Pohjois-Savo/Pohjois-Savo.png"),
+  },
+  {
+    name: "Satakunta",
+    regionCode: "MK04",
+    blazon:
+      "In a shield divided azure and gold, a black, red-armed, crowned bear rampant, holding a silver sword in its paws; the crown …gold; on either side of the bear in the upper field are seven-pointed silver stars. Above the shield rests a ducal coronet.",
+    image: require("@/assets/Satakunta/Satakunta.png"),
+  },
+  {
+    name: "Uusimaa",
+    regionCode: "MK01",
+    blazon:
+      "In a blue field, two silver pine logs with wave crests, with a golden boat with a rudder between them.",
+    image: require("@/assets/Uusimaa/Uusimaa.png"),
+  },
+  {
+    name: "Varsinais-Suomi",
+    regionCode: "MK02",
+    blazon:
+      "In a red field, two crossed golden lances, each bearing a double-tailed blue flag with a gold cross extending to the edges; above the lances, a golden tournament helmet with a crown. The shield is topped with a ducal coronet.",
+    image: require("@/assets/Varsinais-Suomi/Varsinais-Suomi.png"),
+  },
+];
