15b663e50904f040a73c6a5f6210f01b61990087
KarynaMisnik <karinamisnik94@gmail.com>
2026-09-09 11:16:05 +0300

    cards postions on layout done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,39 @@
 import RegionCard from "@/components/RegionCard";
 import { regions } from "@/data/regions";
-import { FlatList, useWindowDimensions } from "react-native";
+import { ScrollView, View, useWindowDimensions } from "react-native";
 
 export default function HomeScreen() {
   const { width } = useWindowDimensions();
-  const columns = width >= 1200 ? 4 : width >= 700 ? 3 : width >= 600 ? 2 : 1;
-  const imageSize = width >= 1200 ? 180 : width >= 700 ? 160 : 150;
+  const gap = 20;
+  const padding = 20;
+  const columns = width >= 1200 ? 5 : width >= 600 ? 3 : width >= 500 ? 2 : 1;
+  const availableWidth = width - padding * 2;
+
+  const cardWidth = (availableWidth - gap * (columns - 1)) / columns;
+
   return (
-    <FlatList
-      data={regions}
-      numColumns={columns}
-      columnWrapperStyle={
-        columns > 1 ? { justifyContent: "space-evenly" } : undefined
-      }
-      contentContainerStyle={{
-        padding: 20,
-      }}
-      renderItem={({ item }) => (
-        <RegionCard name={item.name} image={item.image} imageSize={imageSize} />
-      )}
+    <ScrollView
       style={{
         backgroundColor: "rgba(0, 0, 0, 0.87)",
       }}
-    ></FlatList>
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: gap,
+          padding: padding,
+        }}
+      >
+        {regions.map((region) => (
+          <RegionCard
+            name={region.name}
+            image={region.image}
+            cardWidth={cardWidth}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
