a902419b1b6bc8c2d53ed7389121b4e641956a9e
KarynaMisnik <karinamisnik94@gmail.com>
2026-09-23 21:15:02 +0300

    icon and blazon added to regionCode component. regions.ts updated, location field and addresses added

--- app/(tabs)/region/[regionCode].tsx
+++ app/(tabs)/region/[regionCode].tsx
@@ -1,14 +1,43 @@
 import { regions } from "@/data/regions";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 export default function RegionScreen() {
   const { regionCode } = useLocalSearchParams();
   const region = regions.find((region) => region.regionCode === regionCode);
 
   return (
-    <View>
-      <Text>{region?.name}</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: "rgba(0, 0, 0, 0.87)" }}>
+      <View
+        style={{
+          margin: 10,
+
+          backgroundColor: "white",
+        }}
+      >
+        <View style={{ flexDirection: "row", margin: 8 }}>
+          <Image
+            source={region?.image}
+            style={{
+              width: 50,
+              height: 50,
+              resizeMode: "contain",
+            }}
+          />
+
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 24,
+              height: 60,
+              margin: 8,
+            }}
+          >
+            {region?.name}
+          </Text>
+        </View>
+        <Text style={{ margin: 8 }}>{region?.blazon}</Text>
+      </View>
+    </ScrollView>
   );
 }
--- data/regions.ts
+++ data/regions.ts
@@ -2,132 +2,153 @@ export const regions = [
   {
     name: "Ahvenanmaa",
     regionCode: "MK21",
-    regionDescription:
-      "Åland's coat of arms (a red deer) was mistakenly swapped with Öland's (two roe deer with roses). The error, discovered in th…ife of the islands, which were historically abundant with deer and other animals. It was designed by Ahti Hammarin in 1962.",
+    blazon:
+      "Åland's coat of arms (a red deer) was mistakenly swapped with Öland's (two roe deer with roses). The error, discovered in the 1940s after Åland joined Finland, was left unchanged by Finnish heraldic authorities. Sweden later added a red collar to Öland's deer to avoid further confusion. The deer in Åland's coat of arms represents the wildlife of the islands, which were historically abundant with deer and other animals. It was designed by Ahti Hammarin in 1962.",
     image: require("@/assets/Ahvenanmaa/Ahvenanmaa.png"),
+    location: require("@/assets/Ahvenanmaa/ahvenanmaa-location.png"),
   },
   {
     name: "Etelä-Karjala",
     regionCode: "MK09",
     blazon:
-      "On a shield split horizontally in red and black, a wavy silver bar appears with two clashing arms above it. The right arm, a…holds a curved saber. All elements are silver, except for the golden hilts of the weapons and the joint rings on the armor.",
+      "The crossed arms and weapons are inspired by the historical coat of arms of Karelia, with colors traditional to the region. The wavy bar symbolizes Lake Saimaa and other regional waterways. This coat of arms was created by architect Tuula Taskula and approved by the South Karelia Regional Council on April 2, 1997.",
     image: require("@/assets/Etelä-Karjala/Etelä-Karjala.png"),
+    location: require("@/assets/Etelä-Karjala/etelä-karjala-location.png"),
   },
   {
     name: "Etelä-Pohjanmaa",
     regionCode: "MK14",
     blazon:
-      "In a blue field, three silver ermines running in a row with black-tipped tails.",
+      "The ermines, adapted from the historical Ostrobothnia coat of arms (which originally featured six), symbolize purity and wealth. Northern Ostrobothnia now uses the historical coat of arms without a crown, while Ostrobothnia retains ermines in its current emblem. Central Ostrobothnia, meanwhile, uses a marten-based design from an earlier version. It was created in 1997 by the Regional Council of South Ostrobothnia, the coat of arms was designed under the guidance of heraldic expert Touko Saari. ",
     image: require("@/assets/Etelä-Pohjanmaa/Etelä-Pohjanmaa.png"),
+    location: require("@/assets/Etelä-Pohjanmaa/etelä-pohjanmaa-location.png"),
   },
   {
     name: "Etelä-Savo",
     regionCode: "MK10",
     blazon:
-      "In a black field, a handbow set in tension, all in gold, except for the bowstring, arrowhead, and fletched tail, which are silver.",
+      "The design is a variation of the Savonia region's historical coat of arms, used in its crownless form by North Savonia. Early versions of Savonia's coat of arms showed the bow in the same orientation as South Savonia's, which is considered a heraldic exception. The Savonian bow appears in the coats of arms of Mikkeli and Savonlinna and in the traditional Savonian host banner.",
     image: require("@/assets/Etelä-Savo/Etelä-Savo.png"),
+    location: require("@/assets/Etelä-Savo/etelä-savo-location.png"),
   },
   {
     name: "Kainuu",
     regionCode: "MK18",
     blazon:
-      "In a gold field, a green fess embattled at the top and wavy at the bottom.",
+      "The design reflects Kainuu's natural features: the embattled line symbolizes the region's extensive forests, while the wavy line represents its waterways, both of which have played a significant role in Kainuu's history. It was designed by heraldist Olof Eriksson in 1978 for the Kainuu Regional Council, it was officially adopted as the regional coat of arms during the 1997 provincial reform.",
     image: require("@/assets/Kainuu/Kainuu.png"),
+    location: require("@/assets/Kainuu/kainuu-location.png"),
   },
   {
     name: "Kanta-Häme",
     regionCode: "MK05",
     blazon:
-      "A golden lynx walking on a red field, with black ear tufts; accompanied by three six-pointed stars above the lynx and four roses below, arranged 1:3; all silver. On the shield is a ducal crown.",
+      "The coat of arms of Häme features a golden lynx, symbolizing the area's abundant lynx population and the historical importance of hunting. The stars and roses were added to fill empty spaces, following heraldic tradition. The design was confirmed by the Ministry of the Interior in 1963, based on a model by heraldist Ahti Hammar. Häme’s first known symbol dates back to 1326, and the province's official arms were granted in 1560 by King Gustav Vasa. The lynx appears in the arms of several municipalities and regional emblems.",
     image: require("@/assets/Kanta-Häme/Kanta-Häme.png"),
+    location: require("@/assets/Kanta-Häme/kanta-häme-location.png"),
   },
   {
     name: "Keski-Pohjanmaa",
     regionCode: "MK16",
     blazon:
-      "A silver marten with golden claws and tongue, looking backward, on a blue field",
+      "It is accompanied by three silver nail crosses above and two below. The marten originates from the 1562 coat of arms of historical Ostrobothnia, while the nail crosses symbolize the region’s five original parishes and its skilled craftsmanship. The traditional colors—blue, silver, and gold—also appear in the regional pennant (designed in 1979) and the regional flag adopted in 2000. The current look was designed by heraldist Gustaf von Numers in 1950, it became the region’s official emblem in 1997.",
     image: require("@/assets/Keski-Pohjanmaa/Keski-Pohjanmaa.png"),
+    location: require("@/assets/Keski-Pohjanmaa/keski-pohjanmaa-location.png"),
   },
   {
     name: "Keski-Suomi",
     regionCode: "MK13",
-    blazon: "In a silver field, a black, red-breasted capercaillie.",
+    blazon:
+      "The colors—black, silver, and red—are taken from the emblem of the Central Finland student association at the University of Helsinki, established in 1932. These colors are also used in the traditional Central Finland regional flag. In 1985, the capercaillie was chosen as Central Finland's regional bird. It was designed by herald Ahto Numminen in 1951 for the Central Finland Regional Council, it became the official emblem of the region following the administrative reform of 1997. From 1962 to 1997, it was part of the coat of arms of Central Finland County.",
     image: require("@/assets/Keski-Suomi/Keski-Suomi.png"),
+    location: require("@/assets/Keski-Suomi/keski-suomi-location.png"),
   },
   {
     name: "Kymenlaakso",
     regionCode: "MK08",
     blazon:
-      "In a divided shield, the upper blue field features a silver salmon leaping upward, with golden details, and the lower silver field displays a blue net extending to the edges.",
+      "The coat of arms of Kymenlaakso represents the region and features a rising silver salmon with gold fins and tail on a divided shield. The lower section of the shield depicts a blue net. It was designed by herald Gustaf von Numers in 1951 for the Kymenlaakso Regional Council, the design references the Kymijoki River, a historic and vital salmon river. The colors of blue and silver were chosen to highlight the region’s natural heritage and its connection to the coastal municipalities historically considered part of Uusimaa. The coat of arms was adopted in 1997 after the regional administrative reform and had previously been part of the Kymen Province's heraldry from 1962 to 1997.",
     image: require("@/assets/Kymenlaakso/Kymenlaakso.png"),
+    location: require("@/assets/Kymenlaakso/kymenlaakso-location.png"),
   },
   {
     name: "Lappi",
     regionCode: "MK19",
     blazon:
-      "In a red field, a natural-colored wild man carrying a golden club over his shoulder; a green wreath adorns his forehead and waist. Above the shield is a comital coronet.",
+      "The first official depiction of a woodsman as a symbol of Lapland appeared in 1606, on a coin and in the heraldic chain surrounding the seal of Charles IX, which featured the Lapland coat of arms. In this early version, the woodsman was shown in black, wearing a black fur coat. Later in the century, a variation emerged, depicting the figure nude, with only a wreath of foliage around his waist. Although the core design has remained consistent, the appearance of the Lapland coat of arms has evolved over time. The current version, designed by Ahti Hammar in 1963, was officially confirmed by the Finnish government on January 24, 1963.",
     image: require("@/assets/Lappi/Lappi.png"),
+    location: require("@/assets/Lappi/lappi-location.png"),
   },
   {
     name: "Päijät-Häme",
     regionCode: "MK07",
     blazon:
-      "In a blue field, a mermaid and in the upper right corner, a cuckoo; all in gold.",
+      "The coat of arms of Päijät-Häme, commonly known as Vellamo after the mythological Finnish water deity, embodies the natural and cultural identity of the region. The mermaid figure symbolizes Päijät-Häme's extensive lakes and rolling ridges, highlighting the area’s picturesque landscapes and abundant water resources. Meanwhile, the cuckoo pays homage to the Karelian heritage of the local population, representing their cultural contributions to the region. The coat of arms was designed by Professor Tapani Aartomaa and formally adopted in 1997.",
     image: require("@/assets/Päijät-Häme/Päijät-Häme.png"),
+    location: require("@/assets/Päijät-Häme/päijät-häme-location.png"),
   },
   {
     name: "Pirkanmaa",
     regionCode: "MK06",
-    blazon: "In a golden field, a red, fur-cornered cap.",
+    blazon:
+      "The fur-cornered cap refers to the region's history as an area of hunting and trade, with fur trading being a significant part of the economy of the historical Pirkanmaa people. The colors used in the coat of arms represent two historical regions traditionally associated with Pirkanmaa: gold symbolizes Satakunta, and red symbolizes Häme. The coat of arms was designed by heraldist Gustaf von Numers in 1957 as the symbol for the Pirkanmaa Regional Council, and it became the official coat of arms for the Pirkanmaa region after the 1997 regional reform.",
     image: require("@/assets/Pirkanmaa/Pirkanmaa.png"),
+    location: require("@/assets/Pirkanmaa/pirkanmaa-location.png"),
   },
   {
     name: "Pohjanmaa",
     regionCode: "MK15",
     blazon:
-      "In a red field, a golden royal fasces (the emblem of the House of Vasa) and a chief, also gold, with four red ermines courant arranged 2+2.",
+      "The royal fasces, drawn from Vaasa's coat of arms, symbolizes the historical House of Vasa. The ermines in the chief, inspired by the 1576 historical coat of arms of Ostrobothnia, represent purity and dignity. The coat of arms' colors reflect the Swedish-speaking population's heritage, aligning with Finland's national colors and appearing in the traditional flag of Ostrobothnia's Swedish-speaking community. The emblem of the House of Vasa was also featured in the former Vaasa Province's coat of arms. The coat of arms was designed by heraldist Juhani Vepsäläinen on September 28, 1997.",
     image: require("@/assets/Pohjanmaa/Pohjanmaa.png"),
+    location: require("@/assets/Pohjanmaa/pohjanmaa-location.png"),
   },
   {
     name: "Pohjois-Karjala",
     regionCode: "MK12",
     blazon:
-      "In a red field, two arms issuing from the sides and striking at each other, the right arm armored and wielding a straight sw…eft arm clad in chain mail and holding a curved saber; all in silver, with the weapon hilts in gold; above, a golden crown.",
+      "The coat of arms of North Karelia, derived from the emblem of the historical province of Karelia, omits the ducal crown that once symbolized the province's duchy status. Its design likely originates from the funeral banner used at the burial of King Gustav Vasa of Sweden in 1560, although it may have been created earlier during his reign. The oldest known depiction, from the Armorial suédois (1562), shows only the arms and swords. A crown was later added in Gustav Vasa's tomb in Uppsala Cathedral. The crossed arms reflect Karelia's history as a contested borderland between Sweden and Russia, symbolizing the region's many battles. These elements also feature in Finland's national coat of arms. The current version of the coat of arms was finalized in 1962 by the Ministry of the Interior, with heraldist Ahti Hammar drafting its official description. ",
     image: require("@/assets/Pohjois-Karjala/Pohjois-Karjala.png"),
+    location: require("@/assets/Pohjois-Karjala/pohjois-karjala-location.png"),
   },
   {
     name: "Pohjois-Pohjanmaa",
     regionCode: "MK17",
     blazon:
-      "In a blue field, six running silver ermine, with black-tipped tails, arranged in a 2:2:2 pattern.",
+      "The modern coat of arms of Northern Ostrobothnia features six ermine with black-tipped tails. The ermine symbolizes the importance of the fur trade in the region, which has historical significance. The number six and the pattern reflect a traditional design used for the region, and the ermine motif was established for Northern Ostrobothnia in the late 16th century. It was designed by the heraldic artist Ahti Hammar in 1963 but the coat of arms was officially adopted in 1997, albeit without the count's crown, distinguishing it from the historical coat of arms of the larger region.",
     image: require("@/assets/Pohjois-Pohjanmaa/Pohjois-Pohjanmaa.png"),
+    location: require("@/assets/Pohjois-Pohjanmaa/pohjois-pohjanmaa-location.png"),
   },
   {
     name: "Pohjois-Savo",
     regionCode: "MK11",
     blazon:
-      "On a black shield, a gold bow with a string. All in gold except the bowstring, the arrowhead, and the feathered tail, which … pearls on the crown's rim and three pearls above it, one in the middle and one above each of the outermost visible pearls.",
+      "The bow motif is interpreted as symbolizing Savonia's position as the easternmost border province of Sweden before the incorporation of North Karelia in 1617. This design was first seen at the funeral of King Gustav I in 1560, when the Savonian coat of arms appeared among the banners of Sweden's provinces. It is believed that the design of the provincial coats of arms, including Savonia's, was commissioned by the royal court during the reign of King Gustav, particularly under the influence of his sons, who had an interest in heraldry. The current coat of arms of Savonia was designed by the heraldic artist Ahti Hammari. It was officially adopted in 1962, based on Hammari's design, which solidified the heraldic details and structure of the coat of arms as we know it today.",
     image: require("@/assets/Pohjois-Savo/Pohjois-Savo.png"),
+    location: require("@/assets/Pohjois-Savo/pohjois-savo-location.png"),
   },
   {
     name: "Satakunta",
     regionCode: "MK04",
     blazon:
-      "In a shield divided azure and gold, a black, red-armed, crowned bear rampant, holding a silver sword in its paws; the crown …gold; on either side of the bear in the upper field are seven-pointed silver stars. Above the shield rests a ducal coronet.",
+      "Satakunta's coat of arms is one of Finland's oldest provincial symbols, originating from a 1557 emblem granted by King Gustav Vasa to his son John, Duke of Finland. The bear, symbolizing the wilderness of the duchy's northern regions, initially represented areas north of the Aura River but became specifically associated with Satakunta over time. The bear motif has deep roots, evolving from a 14th-century seal depicting Saint Olaf with a royal orb and axe. It gained prominence as a regional symbol through its use in local crown bailiff seals in the 1800s. Current look of the coat of arms was designed by Ahti Hammari in 1963.",
     image: require("@/assets/Satakunta/Satakunta.png"),
+    location: require("@/assets/Satakunta/satakunta-location.png"),
   },
   {
     name: "Uusimaa",
     regionCode: "MK01",
     blazon:
-      "In a blue field, two silver pine logs with wave crests, with a golden boat with a rudder between them.",
+      "Uusimaa’s coat of arms was created later than those of other Swedish and Finnish provinces, with the concept of Uusimaa as a distinct province emerging around 1600. The earliest example of its coat of arms is from a 1599 runestone by Johannes Bureus. The boat symbol, representing Swedish settlers and Uusimaa's coastal location, was used in various contexts, including on Charles IX’s 1606 coin and in the 1617 Swedish City Law. The wave-crested logs represent the region’s rivers, rather than trade or seafaring. Unlike the historical province (and Uusimaa County), the current province does not use the count's crown above the coat of arms. The crowned coat of arms was used as the symbol of Uusimaa County until 1997, and as part of the South Finland Province's coat of arms from 1997 to 2009. The current look of the coat of arms is based on a design by Ahti Hammari in 1962. ",
     image: require("@/assets/Uusimaa/Uusimaa.png"),
+    location: require("@/assets/Uusimaa/uusimaa-location.png"),
   },
   {
     name: "Varsinais-Suomi",
     regionCode: "MK02",
     blazon:
-      "In a red field, two crossed golden lances, each bearing a double-tailed blue flag with a gold cross extending to the edges; above the lances, a golden tournament helmet with a crown. The shield is topped with a ducal coronet.",
+      "Southwest Finland's coat of arms is one of Finland's oldest, originating in 1557 when King Gustav Vasa granted it to his son John, Duke of Finland. It represented the southern regions of Finland, including Halikko and Piikkiö, south of the Aurajoki River. The knight's helmet symbolizes the ducal court and the region's importance as a center of Swedish rule. By the 19th century, it had become firmly established as Southwest Finland's regional symbol. The modern design of the coat of arms of Southwest Finland was created by Gustaf von Numers.",
     image: require("@/assets/Varsinais-Suomi/Varsinais-Suomi.png"),
+    location: require("@/assets/Varsinais-Suomi/varsinais-suomi-location.png"),
   },
 ];
